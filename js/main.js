0ee54ed02e392eef838c8987ba99a4bc7759f127
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-13 10:28:47 -0300

    ajustes no menu de navegacao da index

--- js/main.js
+++ js/main.js
@@ -1,6 +1,15 @@
 let menu = document.querySelector(".menu")
 let menuNavigation = document.querySelector(".menu-navigation")
+let close = document.querySelector(".close")
 
 menu.addEventListener('click', () => {
+    menuNavigation.classList.toggle("active")
+    menu.classList.toggle("active")
+    close.classList.toggle("active")
+})
+
+close.addEventListener('click', () => {
+    menu.classList.toggle("active")
+    close.classList.toggle("active")
     menuNavigation.classList.toggle("active")
 })
