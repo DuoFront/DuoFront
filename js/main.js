a0f1ee068c006b2c35bf1110aefedb9f159ceec2
paolaguedes <53832972+paolaguedes@users.noreply.github.com>
2021-10-15 16:16:18 -0300

    ajuste na funcao java script

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 let menu = document.querySelector(".menu")
 let menuNavigation = document.querySelector(".menu-navigation")
+let x = document.querySelector(".close")
 
-menu.addEventListener('click', () => {
+function menuLateral() {
     menuNavigation.classList.toggle("active")
-})
+    menu.classList.toggle("active")
+    x.classList.toggle("active")
+}
+
+menu.addEventListener('click', menuLateral)
+
+x.addEventListener('click',  menuLateral)
